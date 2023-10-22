8b3a848ecd7cc9a76b50d164363066d083c228a3
marie <mariegacheri@gmail.com>
2023-10-22 19:50:27 +0300

    forth commit

---
+++ index.js
@@ -0,0 +1,23 @@
+ventListener("DOMContentLoaded", function () {
+    const purchaseButton = document.getElementsByClassName("purchase-button");
+    const remainingTicketsElement = document.getElementsByClassName("remaining-tickets");
+  
+    // Total tickets available and tickets sold
+    let totalTickets = 30;
+    let ticketsSold = 0;
+  
+    // Update the initial remaining tickets value
+    remainingTicketsElement.textContent = totalTickets - ticketsSold;
+  
+    // Add a click event listener to the purchase button
+    purchaseButton.addEventListener("click", function () {
+      if (ticketsSold < totalTickets) {
+        // Decrease available tickets and increase tickets sold
+        ticketsSold++;
+        remainingTicketsElement.textContent = totalTickets - ticketsS
+      } else {
+        alert("Sorry, this show is sold out!");
+      }
+    });
+  });
+  
