0677b1d7fd43cd2fbf212541e07f339b2a257d51
marie <mariegacheri@gmail.com>
2023-10-24 21:34:49 +0300

    sixth  commit

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-ventListener("DOMContentLoaded", function () {
-    const purchaseButton = document.getElementsById("purchase-button");
-    const remainingTicketsElement = document.getElementsById("remaining-tickets");
-  
-    // Total tickets available and tickets sold
-    let totalTickets = 30;
-    let ticketsSold = 0;
-  
-    // Update the initial remaining tickets value
-    remainingTicketsElement.textContent = totalTickets - ticketsSold;
-  
-    // Add a click event listener to the purchase button
-    purchaseButton.addEventListener("click", function () {
-      if (ticketsSold < totalTickets) {
-        // Decrease available tickets and increase tickets sold
-        ticketsSold++;
-        remainingTicketsElement.textContent = totalTickets - ticketsS
-      } else {
-        alert("Sorry, this show is sold out!");
-      }
-    });
-});
+// Define a function to fetch movie details and update the DOM
+function fetchMovieDetails() {
+  fetch('/films/1')
+    .then((response) => response.json())
+    .then((data) => {
+      // Update the DOM with movie details
+      const posterImg = document.getElementById('movie-poster');
+      const title = document.getElementById('movie-title');
+      const runtime = document.getElementById('movie-runtime');
+      const showtime = document.getElementById('movie-showtime');
+      const availableTickets = document.getElementById('movie-tickets');
 
-  
+      posterImg.src = data.poster;
+      title.textContent = data.title;
+      runtime.textContent = `Runtime: ${data.runtime} minutes`;
+      showtime.textContent = `Showtime: ${data.showtime}`;
+      availableTickets.textContent = `Available Tickets: ${data.capacity - data.tickets_sold}`;
+    });
+}
+
+// Define a function to fetch the list of movies and populate the menu
+function fetchMovieList() {
+  fetch('/films')
+    .then((response) => response.json())
+    .then((data) => {
+      // Get the menu element
+      const menu = document.getElementById('films');
+
+      // Populate the menu with movie titles
+      data.forEach((movie) => {
+        const movieItem = document.createElement('li');
+        movieItem.textContent = movie.title;
+        // Add event listener to handle clicks on movie titles (for bonus deliverable)
+        movieItem.addEventListener('click', () => fetchMovieDetails(movie.id));
+        menu.appendChild(movieItem);
+      });
+    });
+}
+
+// Function to handle the "Buy Ticket" button click
+function buyTicket() {
+  // You should implement the logic to buy tickets and update the DOM
+  // based on the core deliverables and bonus deliverables.
+}
+
+// Fetch movie details and movie list when the page loads
+fetchMovieDetails();
+fetchMovieList();
